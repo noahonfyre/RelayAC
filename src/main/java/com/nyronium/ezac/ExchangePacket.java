9c6313825303a29d993320cc7ddcb0f55b11d35f
NoahOnFyre <noahonfyre@gmail.com>
2025-04-10 18:42:08 +0200

    feat: add remaining features

--- src/main/java/com/nyronium/ezac/ExchangePacket.java
+++ src/main/java/com/nyronium/ezac/ExchangePacket.java
@@ -1,4 +1,57 @@
 package com.nyronium.ezac;
 
-public class ExchangePacker {
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.network.NetworkEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+public class ExchangePacket {
+    private final List<String> data;
+
+    public ExchangePacket(List<String> data) {
+        this.data = data;
+    }
+
+    public static void encode(ExchangePacket msg, FriendlyByteBuf buf) {
+        buf.writeVarInt(msg.data.size());
+        for (String mod : msg.data) {
+            buf.writeUtf(mod);
+        }
+    }
+
+    public static ExchangePacket decode(FriendlyByteBuf buf) {
+        int size = buf.readVarInt();
+        List<String> mods = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            mods.add(buf.readUtf());
+        }
+        return new ExchangePacket(mods);
+    }
+
+    public static void handle(ExchangePacket msg, Supplier<NetworkEvent.Context> ctx) {
+        ctx.get().enqueueWork(() -> {
+            ServerPlayer sender = ctx.get().getSender();
+            if(sender == null) return;
+
+            List<String> serverMods = new ArrayList<>();
+            ModList.get().getMods().forEach((modInfo -> serverMods.add(modInfo.getModId())));
+
+            List<String> clientOnlyMods = msg.data;
+            clientOnlyMods.removeAll(serverMods);
+
+            String clientOnlyModString = String.join(";", clientOnlyMods);
+
+            Main.LOGGER.info("Received mod list from client {}:", sender.getName().getString());
+            Main.LOGGER.info(clientOnlyModString);
+            Main.LOGGER.info("Logged in at: {} {} {} in {}", sender.getBlockX(), sender.getBlockY(), sender.getBlockZ(), sender.level().dimension().location());
+        });
+        ctx.get().setPacketHandled(true);
+    }
 }
--- src/main/java/com/nyronium/ezac/Main.java
+++ src/main/java/com/nyronium/ezac/Main.java
@@ -2,45 +2,56 @@ package com.nyronium.ezac;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.forgespi.language.IModInfo;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.simple.SimpleChannel;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod(Ezac.MODID)
-public class Ezac {
-    public static final String MODID = "ezac";
-    private static final Logger LOGGER = LogUtils.getLogger();
+import java.util.ArrayList;
+import java.util.List;
 
-    public Ezac() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+@Mod(Main.ID)
+public class Main {
+    public static final String ID = "ezac";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    private static final String PROTOCOL_VERSION = "1";
+    public static final SimpleChannel CHANNEL = NetworkRegistry.newSimpleChannel(
+            ResourceLocation.fromNamespaceAndPath(ID, "main"),
+            () -> PROTOCOL_VERSION,
+            PROTOCOL_VERSION::equals,
+            PROTOCOL_VERSION::equals
+    );
+
+    private static int packetId = 0;
+
+    public static void registerPackets() {
+        CHANNEL.registerMessage(packetId++,
+                ExchangePacket.class,
+                ExchangePacket::encode,
+                ExchangePacket::decode,
+                ExchangePacket::handle);
+    }
+
+    public Main() {
+        IEventBus modEventBus = MinecraftForge.EVENT_BUS;
 
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
+
+        registerPackets();
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
@@ -52,7 +63,7 @@ public class Ezac {
         LOGGER.info("HELLO from server starting");
     }
 
-    @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    @Mod.EventBusSubscriber(modid = ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModEvents {
 
         @SubscribeEvent
@@ -60,6 +71,18 @@ public class Ezac {
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = ID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+
+        @SubscribeEvent
+        public static void onClientJoin(ClientPlayerNetworkEvent.LoggingIn event) {
+            List<IModInfo> mods = ModList.get().getMods();
+            List<String> modIds = new ArrayList<>();
+            mods.forEach((modInfo) -> modIds.add(modInfo.getModId()));
+            CHANNEL.sendToServer(new ExchangePacket(modIds));
         }
     }
 }
